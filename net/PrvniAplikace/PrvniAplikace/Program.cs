4c5f77eda715297c860c421ec06ca49772d91be2
Jan Purchart <jprace@icloud.com>
2023-05-23 13:38:04 +0200

    add prvniAplikace

---
+++ net/PrvniAplikace/PrvniAplikace/Program.cs
@@ -0,0 +1,13 @@
+﻿using PrvniAplikace;
+
+Zdravic Zdravic;
+Zdravic = new Zdravic();
+
+Zdravic.text = "ahoj uzivateli";
+
+
+Console.WriteLine(Zdravic.Pozdrav("Honzo"));
+
+Zdravic.text = "vitam te tu programatore";
+Console.WriteLine(Zdravic.Pozdrav("Karle"));
+
---
+++ net/PrvniAplikace/PrvniAplikace/Zdravic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrvniAplikace
+{
+    /// <summary>
+    /// Trida reprezentuje zdravic, ktery slouzi ke zdraveni uzivatelu
+    /// </summary>
+    internal class Zdravic
+    {
+        /// <summary>
+        /// text pozdravu
+        /// </summary>
+        public string text;
+        /// <summary>
+        /// pozdravi uzivatele textem pozdravu a jeho jmenem
+        /// </summary>
+        /// <param name="jmeno">Jmeno uzivatele</param>
+        /// <returns>Text s pozdravem</returns>
+        public string Pozdrav(string jmeno)
+        {
+            return string.Format("{0} {1}", text, jmeno);
+
+        }
+    }
+}
