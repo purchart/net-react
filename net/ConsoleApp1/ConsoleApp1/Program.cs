9f84d9d1db06fea566ea510ef08d8aa780ded778
Jan Purchart <jprace@icloud.com>
2023-05-23 13:38:40 +0200

    add consoleApp1

---
+++ net/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("zadej jmeno: ");
+string jmeno = Console.ReadLine();
+Console.WriteLine("zadej prijmeni: ");
+string prijmeni = Console.ReadLine();
+Console.WriteLine("zadej vek: ");
+string vek = Console.ReadLine();
+Console.WriteLine(jmeno.ToUpper() + " " + prijmeni.ToUpper());
+Console.WriteLine($"za rok ti bude {vek + 1}");
